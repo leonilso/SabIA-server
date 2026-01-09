5e0a6cd62d62dcf06ee504d904bdaa02d244f330
leonilso <wrublak.leonilso@escola.pr.gov.br>
2026-01-09 13:38:10 -0300

    ajustando cors do webhook

--- src/api/routes/webhook.routes.js
+++ src/api/routes/webhook.routes.js
@@ -1,21 +1,31 @@
 import express from "express";
+import cors from "cors";
 import stripe from "../../config/stripe.js";
 import PagamentoService  from "../../services/pagamento.service.js";
 
 const router = express.Router();
 const service = new PagamentoService();
 
+// Middleware CORS separado
+const corsOptions = { origin: "https://hooks.stripe.com" };
+
 router.post(
   "/",
+  cors(corsOptions),
   express.raw({ type: "application/json" }),
   async (req, res) => {
     const signature = req.headers["stripe-signature"];
 
-    const event = stripe.webhooks.constructEvent(
-      req.body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
 
     if (event.type === "checkout.session.completed") {
       await service.confirmarPagamento(event.data.object);
